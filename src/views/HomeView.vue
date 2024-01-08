853128ba1ccb25bfaa792114546a15ac36144967
Aleyana <annesophie.elsayyed.13560@stu.he-ferrer.eu>
2024-01-08 19:06:19 +0100

    a noto project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,20 @@
   <main class="home">
     <h1>Home</h1>
     <router-link to="/about">About</router-link>
+
+    <!-- <spline :scene=YOUR_SCENE_OBJECT_HERE /> -->
   </main>
 </template>
+
+<script>
+// import Spline from './vue-spline';
+
+ export default{
+  components: {
+    // Spline
+  }
+ } 
+
+</script>
 
 
